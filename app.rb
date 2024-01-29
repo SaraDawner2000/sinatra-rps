cb7b02df0e7d50930210eaaf1ead418fdd082336
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-01-29 19:32:16 +0000

    create app skeleton

--- app.rb
+++ app.rb
@@ -2,8 +2,14 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:rules)
+end
+get("/rock") do
+  erb(:rock, {:layout => :layout})
+end
+get("/paper") do
+  erb(:paper, {:layout => :layout})
+end
+get("/scissors") do
+  erb(:scissors, {:layout => :layout})
 end
