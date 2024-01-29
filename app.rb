54b5eda31534e6644125239fe1d4397f2c159179
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-01-29 20:09:24 +0000

    Create rock, paper, scissors

--- app.rb
+++ app.rb
@@ -4,12 +4,30 @@ require "sinatra/reloader"
 get("/") do
   erb(:rules)
 end
+
 get("/rock") do
+  options = ["rock", "paper", "scissors"]
+  @player = "rock"
+  computer_choice_index = rand(0..2)
+  @computer = options[computer_choice_index]
+
   erb(:rock, {:layout => :layout})
 end
+
 get("/paper") do
+  options = ["rock", "paper", "scissors"]
+  @player = "paper"
+  computer_choice_index = rand(0..2)
+  @computer = options[computer_choice_index]
+
   erb(:paper, {:layout => :layout})
 end
+
 get("/scissors") do
+  options = ["rock", "paper", "scissors"]
+  @player = "scissors"
+  computer_choice_index = rand(0..2)
+  @computer = options[computer_choice_index]
+
   erb(:scissors, {:layout => :layout})
 end
